a411bb1966dff97f4b91001c5718beef22c0e9e4
SVarshithReddyA <varshith0905@gmail.com>
2024-11-14 00:52:46 +0000

    revert trugger again

--- function_app.py
+++ function_app.py
@@ -1,40 +1,25 @@
 import azure.functions as func
 import logging
-from azure.storage.blob import BlobServiceClient
-from werkzeug.utils import secure_filename
-import os
 
-# Initialize Function App
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-# Retrieve connection string from environment variable
-connection_string = "DefaultEndpointsProtocol=https;AccountName=distributedsys;AccountKey=jLyjj/LyKtRVjlVfdawM5uaBhQZfBUiec+9xxiPxgWVXzn/yng4Rp6/XZojKSjVWZbejiBXuZ+MS+ASteWFJaA==;EndpointSuffix=core.windows.net"
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-UPLOAD_CONTAINER = "filestore"
-
-# Define the HTTP trigger function
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function for image upload processed a request.')
+    logging.info('Python HTTP trigger function processed a request.')
 
-    try:
-        # Extract file from the request
-        image_file = req.files['file']
-        file_name = image_file.filename
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
 
-        # Upload to Blob Storage
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        blob_client = blob_service_client.get_blob_client(container=UPLOAD_CONTAINER, blob=file_name)
-        blob_client.upload_blob(image_file.stream, overwrite=True)
-
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
         return func.HttpResponse(
-            f"Image '{file_name}' uploaded successfully to Blob Storage.",
-            status_code=200
-        )
-
-    except Exception as e:
-        logging.error(f"Error uploading image: {e}")
-        return func.HttpResponse(
-            f"Failed to upload image. Error: {str(e)}",
-            status_code=500
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
         )
